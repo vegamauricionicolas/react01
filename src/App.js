f33e6b1d0f95669b9c7f522661407fbebde9f680
Mauricio <vegamauricionicolas@gmail.com>
2020-08-03 14:39:48 -0300

    Subi el directorio my-app que es un proyecto de react js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Contador from './components/Contador';
+
+function App() {
+  return (
+    <div>
+      <h1>Hola Mundo REACT</h1>
+      <Contador/>
+      
+    </div>
+  );
+}
+
+export default App;
